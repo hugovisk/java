25f94c6b0b2eed9919e79b015f9e1dc68db34b62
Hugo Melo <hugovisk@gmail.com>
2019-10-10 14:40:05 -0300

    sync projeto

---
+++ Aula05_03/src/negocio/Multiplicacao.java
@@ -0,0 +1,14 @@
+
+package negocio;
+
+/**
+ *
+ * @author dreis
+ */
+public class Multiplicacao extends Calculo{
+
+    @Override
+    public float calcular(float valor1, float valor2) {
+        return valor1 * valor2; 
+    }
+}
---
+++ Aula05_03/src/negocio/Subtracao.java
@@ -0,0 +1,14 @@
+
+package negocio;
+
+/**
+ *
+ * @author dreis
+ */
+public class Subtracao extends Calculo{
+
+    @Override
+    public float calcular(float valor1, float valor2) {
+        return valor1 - valor2; 
+    }
+}
